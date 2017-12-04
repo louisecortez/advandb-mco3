4e21f8685beab36fead8b542f598b5a14e269bcc
louisecortez <louise_cortez@dlsu.edu.ph>
2017-12-05 00:31:07 +0800

    DB Connection

--- src/dbconnection/Connector.java
+++ src/dbconnection/Connector.java
@@ -1,5 +1,65 @@
 package dbconnection;
 
-public class Connector {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+
+
+public class Connector {
+	private static String url;
+	private static String schema;
+	private static String driver;
+	private static String username;
+	private static String password;
+	
+	public Connector(String url, String schema, String driver, String username, String password) {
+		this.url = url;
+		this.schema = schema;
+		this.driver = driver;
+		this.username = username;
+		this.password = password;
+	}
+	
+	public static Connection getConnection() {
+		Connection connection = null;
+		
+		try {
+			Class.forName(driver).newInstance();
+			connection = DriverManager.getConnection(url + schema, username, password);
+			return connection;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return connection;
+	}
+	
+	public static void executeStatement(String statement) {
+		Connection connection = getConnection();
+		
+		try {
+			PreparedStatement pst = connection.prepareStatement(statement);
+			pst.execute(statement);
+			connection.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static ResultSet executeQuery(String query) {
+		ResultSet result = null;
+		Connection connection = Connector.getConnection();
+		
+		try {
+			Statement st = connection.createStatement();
+			result = st.executeQuery(query);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
 }
