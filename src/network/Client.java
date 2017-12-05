7977abddd3abb335b3aae1556be314894d133e53
louisecortez <louise_cortez@dlsu.edu.ph>
2017-12-05 15:32:55 +0800

    Client attributes

--- src/network/Client.java
+++ src/network/Client.java
@@ -1,5 +1,29 @@
 package network;
+import view.MainFrame;
+
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class Client {
-
+	public static final String FIRST_NODE = "EuropeAmerica";
+	public static final String SECOND_NODE = "AsiaAfrica";
+	public static final String THIRD_NODE = "AllRegions";
+	
+	private ServerSocket socket;
+	private String server; // server IP address
+	private String clientName;
+	private String dbName;
+	private int sharedPort = 1234; // change to shared port
+	private int portNo;
+	
+	private MainFrame mainFrame;// gui per client
+	
+	public Client(MainFrame mainFrame, String server, String clientName) throws IOException {
+		this.mainFrame = mainFrame;
+		this.server = server;
+		this.clientName = clientName;
+		
+		socket = new ServerSocket(1234); // change port number
+		socket.setSoTimeout(10000); // 10 secon
+	}
 }
