27c46f6f8d29e3d54e999120dc870fd05951846d
Sophia Therese T. Rivera <sophia_rivera@dlsu.edu.ph>
2017-12-08 03:16:24 +0800

    entity 2.0

--- src/network/Client.java
+++ src/network/Client.java
@@ -323,14 +323,13 @@ public class Client {
 									try {
 										while(rs.next()){
 											Entity cur = new Entity(
-													rs.getInt(1),
+													rs.getString(1),
 													rs.getString(2),
 													rs.getString(3),
 													rs.getString(4),
 													rs.getString(5),
-													rs.getString(6),
-													rs.getInt(7),
-													rs.getInt(8));
+													rs.getInt(6),
+													rs.getInt(7));
 											entities.add(cur);
 										}
 										System.out.println("MAY RESULTSSSS");
@@ -469,14 +468,13 @@ public class Client {
 				try {
 					while(rs.next()){
 						Entity cur = new Entity(
-								rs.getInt(1),
+								rs.getString(1),
 								rs.getString(2),
 								rs.getString(3),
 								rs.getString(4),
 								rs.getString(5),
-								rs.getString(6),
-								rs.getInt(7),
-								rs.getInt(8));
+								rs.getInt(6),
+								rs.getInt(7));
 								
 						e.add(cur);
 					}
