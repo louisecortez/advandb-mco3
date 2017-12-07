d8e53ddca989662f7521db846ba60d1dd1cf13ae
Sophia Therese T. Rivera <sophia_rivera@dlsu.edu.ph>
2017-12-08 02:24:27 +0800

    generally, dinagdagan

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -10,10 +10,12 @@ public class Driver {
  	public static void main(String[] args) throws IOException {
 
  		
-			Server s = new Server();
-			
-			new FrameIPAddress(new Controller(s));
-			s.accept();
+			//Server s = new Server();
+			Controller c = new Controller();
+			new FrameIPAddress(c);
+			new FrameIPAddress(c);
+			new FrameIPAddress(c);
+			//s.accept();
 			
 			/*
 			Controller controller = new Controller(frameIP);
