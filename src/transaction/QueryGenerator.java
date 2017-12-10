6a9beee2e30c4bd560300c1ee6cd9e17ae8b6007
louisecortez <louise_cortez@dlsu.edu.ph>
2017-12-10 20:32:21 +0800

    Complete local and global transactions

--- src/transaction/QueryGenerator.java
+++ src/transaction/QueryGenerator.java
@@ -18,6 +18,14 @@ public class QueryGenerator {
 	}
 	
 	public static String generateUpdate(String table, String country, String series, int year, double data) {
+		if(table.equals(Client.FIRST_NODE)) {
+			table = "dbmco3.europe_america";
+		} else if(table.equals(Client.SECOND_NODE)) {
+			table = "dbmco3.asia_africa";
+		} else if(table.equals(Client.THIRD_NODE)) {
+			table = "dbmco3.all_regions";
+		}
+		
 		String query = "UPDATE " + table +
 				" SET `Data` = " + data + 
 				" WHERE CountryName = '" + country + 
@@ -28,6 +36,14 @@ public class QueryGenerator {
 	}
 	
 	public static String generateInsert(String table, String country, String series, int year, double data) {
+		if(table.equals(Client.FIRST_NODE)) {
+			table = "dbmco3.europe_america";
+		} else if(table.equals(Client.SECOND_NODE)) {
+			table = "dbmco3.asia_africa";
+		} else if(table.equals(Client.THIRD_NODE)) {
+			table = "dbmco3.all_regions";
+		}
+		
 		String query = "INSERT INTO " + table +
 				"(CountryCode, CountryName, Region, SeriesCode, SeriesName, YearC, `Data`) " + 
 				"VALUES ('AAA', '" + country + "', 'Sample Region', 'ISO.AWC.XAYS.AQ', '" + series + "', " +
@@ -37,6 +53,14 @@ public class QueryGenerator {
 	}
 	
 	public static String generateDelete(String table, String country, String series, int year, double data) {
+		if(table.equals(Client.FIRST_NODE)) {
+			table = "dbmco3.europe_america";
+		} else if(table.equals(Client.SECOND_NODE)) {
+			table = "dbmco3.asia_africa";
+		} else if(table.equals(Client.THIRD_NODE)) {
+			table = "dbmco3.all_regions";
+		}
+		
 		String query = "DELETE FROM " + table +
 				" WHERE CountryName = '" + country + 
 				"' AND SeriesName = '" + series + 
